159d42d6e8036931a752d95c80b9b3021004104a
SaadSamir7 <132520150+SaadSamir7@users.noreply.github.com>
2023-08-21 02:52:25 +0300

    functions 2

---
+++ printf_pointer.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _printf_pointer - prints an hexgecimal number.
+ * @list: arguments.
+ * Return: The number of characters printed..
+ */
+int _printf_pointer(va_list list)
+{
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int b;
+	int i;
+
+	p = va_arg(list, void*);
+	if (p == NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
+
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	b = hex(a);
+	return (b + 2);
+}
