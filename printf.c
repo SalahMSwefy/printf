60190b1e9cd5b2ed89127b243975ccb88e195d0f
SaadSamir7 <132520150+SaadSamir7@users.noreply.github.com>
2023-08-21 02:31:16 +0300

    adding printf.c

--- printf.c
+++ printf.c
@@ -9,18 +9,17 @@ int _printf(const char *const format, ...)
 {
 	paramter func[] = {
 		{"s", _printf_string}, {"c", _printf_char}, {"%", _printf_perc},
-		 {"i", _printf_int}, {"d", _printf_int}, {"r", _printf_str_rev}, 
-		 {"R", _printf_rot13}, {"b", _printf_binary}, {"u", _printf_unsigned}, 
-		 {"o", _printf_octal}, {"x", _printf_hexa}, {"X", _printf_HEXA}, 
+		 {"i", _printf_int}, {"d", _printf_int}, {"r", _printf_str_rev},
+		 {"R", _printf_rot13}, {"b", _printf_binary}, {"u", _printf_unsigned},
+		 {"o", _printf_octal}, {"x", _printf_hexa}, {"X", _printf_HEXA},
 		 {"S", _printf_ex_str}, {"p", _printf_pointer}, {NULL, NULL}};
 
 	va_list list;
-	int i = 0, j, len = 0, flag = 1;
+	int i = 0, j, len = 0;
 
 	va_start(list, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
@@ -32,15 +31,9 @@ int _printf(const char *const format, ...)
 				{
 					len += func[j].f(list);
 					i = i + 2;
-					flag = 0;
 					break;
 				}
 				j++;
-			}
-			if (flag)
-			{
-				_putchar('x');
-				len++;
 			}
 		}
 		_putchar(format[i]);
